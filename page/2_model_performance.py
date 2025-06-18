135c582af084fd35f273bb5634db606b7b5233f8
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 10:47:41 +0700

    Update 2_model_performance.py

--- page/2_model_performance.py
+++ page/2_model_performance.py
@@ -24,3 +24,28 @@ y_pred = model.predict(X_test)
 joblib.dump(model, "model.pkl")
 
 st.code(classification_report(y_test, y_pred), language='text')
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+import matplotlib.pyplot as plt
+
+# Confusion Matrix
+st.subheader("Confusion Matrix")
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+fig, ax = plt.subplots()
+disp.plot(ax=ax)
+st.pyplot(fig)
+
+# ROC Curve
+st.subheader("ROC Curve")
+y_prob = model.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, y_prob, pos_label=y.unique()[1])
+roc_auc = auc(fpr, tpr)
+fig, ax = plt.subplots()
+ax.plot(fpr, tpr, label=f"AUC = {roc_auc:.2f}")
+ax.plot([0, 1], [0, 1], linestyle="--", color="gray")
+ax.set_xlabel("False Positive Rate")
+ax.set_ylabel("True Positive Rate")
+ax.legend()
+st.pyplot(fig)
+
