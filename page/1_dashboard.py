16a95ce31b18fa1f9f450e17a203aed6b4305000
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 10:42:33 +0700

    Update 1_dashboard.py

--- page/1_dashboard.py
+++ page/1_dashboard.py
@@ -1,1 +1,19 @@
 
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("📊 Dashboard - Exploratory Data Analysis")
+df = pd.read_csv("data/data.csv")
+
+st.subheader("Tampilan Data")
+st.dataframe(df)
+
+st.subheader("Distribusi Target (Hasil)")
+st.bar_chart(df["Hasil"].value_counts())
+
+st.subheader("Usia vs Hasil")
+fig, ax = plt.subplots()
+sns.histplot(data=df, x="Usia", hue="Hasil", multiple="stack", ax=ax)
+st.pyplot(fig)
